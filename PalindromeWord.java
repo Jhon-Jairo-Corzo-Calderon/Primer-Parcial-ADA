b7e5eb84fd69ee551beb9d654dc29bc22678b047
Jhon <jhon.corzo.2020@upb.edu.co>
2021-09-06 15:52:30 -0500

    Metodo main

--- PalindromeWord.java
+++ PalindromeWord.java
@@ -1,2 +1,27 @@
+import java.util.Scanner;
+
 public class PalindromeWord {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String word ;
+        boolean loop= true, palindrome;
+        System.out.println("Ingrese la cantidad de palabras (NUMERO ENTERO) a evaluar:");
+
+        for (int i = sc.nextInt(); i > 0; i--) {
+            System.out.println("Ingrese palabra para verificar si es polindroma: ");
+            word = sc.next();
+            palindrome = isPalindrome(word);
+            if(palindrome )
+                System.out.println( word + " es palindrome");
+            else
+                System.out.println( word + " NO es palindrome");
+        }
+
+    }
+    public static boolean isPalindrome(String word){
+
+
+        return true;
+    }
+
 }
